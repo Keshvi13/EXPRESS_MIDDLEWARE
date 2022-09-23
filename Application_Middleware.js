0ac0b1bf5c2d89217c2cadcc6fac8d87c3910e53
keshviiipatel <keshviiipatel@gmail.com>
2022-09-23 20:41:07 +0530

    new repositry add

---
+++ Application_Middleware.js
@@ -0,0 +1,15 @@
+var express = require('express');
+const { nextTick } = require('process');
+var app=express()
+
+app.use('/',(req,res,next)=>{
+     console.log("Middleware called");
+     next();
+})
+
+app.get('/user',(req,res)=>{
+    console.log("/abc request called");
+    res.send('welcome app use');
+})
+
+app.listen(3000,()=>{console.log("server start @localhost:3000")})
